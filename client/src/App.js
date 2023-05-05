b8d1f8d08ee4e0671b78da62b3f2cfe728b8575b
dickenzoh <dickenzoh@gmail.com>
2023-05-05 05:03:22 +0300

    added the page structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageDual from "./Pages/PageDual/PageDual";
+import PageOne from "./Pages/PageOne/PageOne";
+import PageTwo from "./Pages/PageTwo/PageTwo";
+import ProfilePage from "./Pages/ProfilePage/ProfilePage";
+import AdminPage from "./Pages/AdminPage/AdminPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Routes>
+          <Route path="/" exact element={<PageOne />} />
+          <Route path="/pagedual" exact element={<PageDual />} />
+          <Route path="/pagetwo" exact element={<PageTwo />} />
+          <Route path="/admin" exact element={<AdminPage />} />
+          <Route path="/profile" exact element={<ProfilePage />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ client/src/Pages/PageDual/PageDual.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageDual = () => {
+  return <div>Dual Page</div>;
+};
+
+export default PageDual;
