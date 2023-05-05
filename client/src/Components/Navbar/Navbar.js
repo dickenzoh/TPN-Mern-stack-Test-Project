3aff2527d1630b5938c26f6f3a068d74ec4fee48
dickenzoh <dickenzoh@gmail.com>
2023-05-05 09:50:30 +0300

    added profile form page

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -13,6 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import TPNlogo from "../../images/TPN-logo.png";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 const pages = ["Page 1", "Page 2", "Dual Page", "Admin Page"];
 const settings = ["Profile", "Logout"];
@@ -20,6 +21,8 @@ const settings = ["Profile", "Logout"];
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const navigate = useNavigate();
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -36,6 +39,18 @@ function Navbar() {
     setAnchorElUser(null);
   };
 
+  const handleSettings = (settings) => {
+    console.log(settings);
+    if (settings === "Profile") {
+      navigate("/profile");
+    }
+    if (settings === "Logout") {
+      navigate("/auth");
+    }
+
+    handleCloseUserMenu();
+  };
+
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
@@ -47,7 +62,7 @@ function Navbar() {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <AppBar position="fixed" style={{ width: "100%" }}>
+      <AppBar position="static" style={{ width: "100%" }}>
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <img
@@ -132,7 +147,12 @@ function Navbar() {
                 onClose={handleCloseUserMenu}
               >
                 {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <MenuItem
+                    key={setting}
+                    onClick={() => {
+                      handleSettings(setting);
+                    }}
+                  >
                     <Typography textAlign="center">{setting}</Typography>
                   </MenuItem>
                 ))}
--- client/src/Pages/ProfilePage/ProfilePage.js
+++ client/src/Pages/ProfilePage/ProfilePage.js
@@ -1,7 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, Button, Container, Typography } from "@material-ui/core";
+import { Grid, IconButton } from "@mui/material";
+import Input from "../Auth/Input";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "",
+    confirmPassword: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Submit form data to API or backend
+    console.log(formData);
+  };
+
+  const handleImageUpload = () => {
+    console.log("uploaded");
+  };
+
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPass) => !prevShowPass);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Edit Profile
+        </Typography>
+        <IconButton onClick={handleImageUpload} sx={{ p: 0 }}>
+          <Avatar alt="Dickens Kinoti" src="/static/images/avatar/2.jpg" />
+        </IconButton>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <>
+              <Input
+                name="firstName"
+                label="First Name"
+                handleChange={handleChange}
+                autoFocus
+                half
+              />
+              <Input
+                name="lastName"
+                label="Last Name"
+                handleChange={handleChange}
+                half
+              />
+              <Input
+                name="email"
+                label="Email Address"
+                handleChange={handleChange}
+                type="email"
+              />
+              <Input
+                name="password"
+                label="Password"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+              <Input
+                name="confirmPassword"
+                label="Confirm Password"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+            </>
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Save Changes
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default ProfilePage;
