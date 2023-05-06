2aa5c4683ad57e5eae15de779fe224739d03f300
dickenzoh <dickenzoh@gmail.com>
2023-05-06 09:59:17 +0300

    added database connection

--- client/src/Pages/AdminPage/AdminPage.js
+++ client/src/Pages/AdminPage/AdminPage.js
@@ -2,21 +2,66 @@ import React from "react";
 import UserList from "../../Components/Table/Table";
 
 const users = [
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
-  { firstName: "John", lastName: "Mark", email: "johnmark@gmail.com" },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "admin",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "user",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "admin",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "user",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "admin",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "user",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "admin",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "user",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "admin",
+  },
+  {
+    firstName: "John",
+    lastName: "Mark",
+    email: "johnmark@gmail.com",
+    role: "user",
+  },
 ];
 const AdminPage = () => {
   return <UserList users={users} />;
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import postRoutes from "./routes/posts.js";
+// import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// app.use("/posts", postRoutes);
+// app.use("/user", userRoutes);
+
+app.use("/", (req, res) => {
+  res.send("Hello from TPN API art by dickenzoh");
+});
+
+const PORT = process.env.PORT || 3008;
+
+mongoose
+  .connect(process.env.CONN_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
